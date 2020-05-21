00d805e4723154c61b216eaa31f944ecba710282
mbeckers <maximilian.beckers@embl.de>
2020-05-21 15:39:05 +0200

    python version updated

--- multi_model.py
+++ multi_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import Bio.PDB
 from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
 class MultiModel:
 
@@ -8,88 +9,102 @@ class MultiModel:
     coord_array = [];
     tsne_embedding = [];
     pca_embedding = [];
+    explained_variances = [];
     umap_embedding = [];
     clusters = [];
 
     #************************************
-    def read_pdbs(self, filenames):
+    def read_pdbs(self, filenames, align=True, CA=False):
 
         print("Loading PDB files ...");
 
         for tmp_file in filenames:
             self.coordinates.append(Bio.PDB.PDBParser().get_structure('hurz', tmp_file)[0]);
 
-
-    #************************************
-    def align_pdbs(self):
-
-        print("Everything aligned to first model...")
-        ref_model = self.coordinates[0];
-        num_structures = len(self.coordinates);
+        #align the structures
+        if align:
+            print("Everything aligned to first model...")
+            ref_model = self.coordinates[0];
+            num_structures = len(self.coordinates);
 
 
-        for model_ind in range(num_structures):
+            for model_ind in range(num_structures):
 
-            alt_model = self.coordinates[model_ind];
+                alt_model = self.coordinates[model_ind];
 
-            # Build paired lists of c-alpha atoms, ref_atoms and alt_atoms
-            ref_atoms = []
-            alt_atoms = []
-            for (ref_chain, alt_chain) in zip(ref_model, alt_model):
-                for ref_res, alt_res in zip(ref_chain, alt_chain):
-                    assert ref_res.resname == alt_res.resname
-                    assert ref_res.id == alt_res.id
+                # Build paired lists of c-alpha atoms, ref_atoms and alt_atoms
+                ref_atoms = []
+                alt_atoms = []
+                for (ref_chain, alt_chain) in zip(ref_model, alt_model):
+                    for ref_res, alt_res in zip(ref_chain, alt_chain):
+                        assert ref_res.resname == alt_res.resname
+                        assert ref_res.id == alt_res.id
 
-                    # CA = alpha carbon
-                    aa = True;
-                    try:
-                        ref_res['CA'];
-                    except:
-                        aa = False;
+                        # CA = alpha carbon
+                        aa = True;
+                        try:
+                            ref_res['CA'];
+                        except:
+                            aa = False;
 
-                    if aa:
-                        ref_atoms.append(ref_res['CA']);
-                        alt_atoms.append(alt_res['CA']);
-
-
-            # Align these paired atom lists:
-            super_imposer = Bio.PDB.Superimposer()
-            super_imposer.set_atoms(ref_atoms, alt_atoms)
+                        if aa:
+                            ref_atoms.append(ref_res['CA']);
+                            alt_atoms.append(alt_res['CA']);
 
 
-            if (model_ind == 0):
-                # Check for self/self get zero RMS, zero translation
-                # and identity matrix for the rotation.
-                assert np.abs(super_imposer.rms) < 0.0000001
-                assert np.max(np.abs(super_imposer.rotran[1])) < 0.000001
-                assert np.max(np.abs(super_imposer.rotran[0]) - np.identity(3)) < 0.000001
-            else:
-                # Update the structure by moving all the atoms in
-                # this model (not just the ones used for the alignment)
-                super_imposer.apply(alt_model.get_atoms())
+                # Align these paired atom lists:
+                super_imposer = Bio.PDB.Superimposer()
+                super_imposer.set_atoms(ref_atoms, alt_atoms)
 
-    #*************************************
-    def make_vector_representation(self):
 
-        self.coord_array = np.zeros((0,0));
+                if (model_ind == 0):
+                    # Check for self/self get zero RMS, zero translation
+                    # and identity matrix for the rotation.
+                    assert np.abs(super_imposer.rms) < 0.0000001
+                    assert np.max(np.abs(super_imposer.rotran[1])) < 0.000001
+                    assert np.max(np.abs(super_imposer.rotran[0]) - np.identity(3)) < 0.000001
+                else:
+                    # Update the structure by moving all the atoms in
+                    # this model (not just the ones used for the alignment)
+                    super_imposer.apply(alt_model.get_atoms())
+
+
+        #make vector representation of coordinates
+        self.coord_array = [];
         for tmp_model in self.coordinates:
 
-            tmp_coord = np.zeros((0));
+            tmp_coord = [];
             for tmp_chain in tmp_model:
                 for tmp_res in tmp_chain:
-                    for tmp_atom in tmp_res:
+                    if(CA):
+                        try:#if CA atom exists, append it
+                            tmp_atom = tmp_res['CA'];
+                            tmp_coord.append(tmp_atom.get_coord());
+                        except:
+                            pass
+                    else:
+                        #append all atoms
+                        for tmp_atom in tmp_res:
+                            tmp_coord.append( tmp_atom.get_coord());
 
-                        np.append(tmp_coord, tmp_atom.get_vector());
+            tmp_coord = np.asarray(tmp_coord).flatten();
+            self.coord_array.append( tmp_coord);
 
-            np.append(self.coord_array, tmp_coord, axis=1)
+        self.coord_array = np.asarray(self.coord_array)
 
     #**************************************
     def do_pca_embedding(self):
 
         from sklearn.decomposition import PCA
+        pca = PCA();
+        pca.fit(self.coord_array);
+        self.explained_variances = pca.explained_variance_ratio_
+
         pca = PCA(n_components=2);
         pca.fit(self.coord_array);
-        self.pca_embedding = pca.fit_transform;
+        self.pca_embedding = pca.fit_transform(self.coord_array);
+
+        print(self.explained_variances);
 
     #**************************************
     def do_tsne_embedding(self):
@@ -109,6 +124,20 @@ class MultiModel:
 
         from sklearn.cluster import KMeans
         self.clusters = KMeans(n_clusters=num_clusters, random_state=0).fit(self.coordinates);
+
+    #***************************************
+    def make_plots(self):
+
+
+        #plot pca
+        plt.scatter(self.pca_embedding[0,:], self.pca_embedding[1,:], c=self.clusters.labels)
+        plt.title('PCA embedding of random colours');
+        plt.show()
+
+        #plot explained variances
+        plt.plot(range(self.pca_embedding.shape[1]), self.explained_variances);
+        plt.title('Explained variances per principal component');
+
 
     #***************************************
     def write_pdbs(self):
--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from multi_model import MultiModel
 files = ["6sae.pdb", "6sag.pdb"];
 
 m = MultiModel();
-m.read_pdbs(files)
-m.align_pdbs();
+m.read_pdbs(files);
+m.do_pca_embedding();
